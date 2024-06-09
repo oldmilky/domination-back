fe445fa5b1dcf19ba4d55377b4f2e0a32837998c
Родион Стрелков <oldmilky>
2024-06-09 18:34:34 +0600

    hotfix: prod start

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api');
+  app.setGlobalPrefix('');
+  // app.setGlobalPrefix('api') for localhost;
   app.enableCors();
   await app.listen(4200);
 }
